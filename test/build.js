c23bdcbe3131551e10dc4766a0b39a7da664015b
bc022699 <benjamincompton@gmail.com>
2018-03-22 16:48:20 -0400

    Update whitespace in build.js for consistency

--- test/build.js
+++ test/build.js
@@ -7,62 +7,62 @@ const rollupReplace = require('rollup-plugin-replace');
 const compile = require('../compiler-io/');
 
 const buildComponentLibrary = async () => {
-    await compile({
-        paths: ['./component-library/src/**/*.js'],
-        vue: {
-            out: './component-library/dist/vue/'
-        },
-        react: {
-            out: './component-library/dist/react/'
-        }
-    });
+  await compile({
+    paths: ['./component-library/src/**/*.js'],
+    vue: {
+      out: './component-library/dist/vue/'
+    },
+    react: {
+      out: './component-library/dist/react/'
+    }
+  });
 };
 
 let cache;
 
 const buildApp = async library => {
-    const inputOptions = {
-        input: `./test/app/${library}/src/app.js`,
-        cache,
-        plugins: [
-            rollupResolve({ jsnext: true, browser: true }),
-                rollupCommonJs(),
-                rollupReplace({
-                delimiters: ['', ''],
-                'process.env.NODE_ENV': JSON.stringify('development'), // or 'production'
-            }),
-            rollupBuble(),
-        ],
-    };
+  const inputOptions = {
+    input: `./test/app/${library}/src/app.js`,
+    cache,
+    plugins: [
+      rollupResolve({ jsnext: true, browser: true }),
+          rollupCommonJs(),
+          rollupReplace({
+          delimiters: ['', ''],
+          'process.env.NODE_ENV': JSON.stringify('development'), // or 'production'
+      }),
+      rollupBuble(),
+    ],
+  };
 
-    const outputOptions = {
-        format: 'iife',
-        file: `./test/app/${library}/dist/app.js`,
-        name: 'app',
-        strict: true,
-        sourcemap: false,
-    };
+  const outputOptions = {
+    format: 'iife',
+    file: `./test/app/${library}/dist/app.js`,
+    name: 'app',
+    strict: true,
+    sourcemap: false,
+  };
 
-    const buildBundle = async () => {
-        // create a bundle
-        const bundle = await rollup.rollup(inputOptions);
+  const buildBundle = async () => {
+    // create a bundle
+    const bundle = await rollup.rollup(inputOptions);
 
-        // generate code and a sourcemap
-        const { code, map } = await bundle.generate(outputOptions);
+    // generate code and a sourcemap
+    const { code, map } = await bundle.generate(outputOptions);
 
-        // or write the bundle to disk
-        await bundle.write(outputOptions);
-    }
+    // or write the bundle to disk
+    await bundle.write(outputOptions);
+  }
 
-    await buildBundle();
+  await buildBundle();
 };
 
 const buildAll = async () => {
-    await buildComponentLibrary();
-    await [
-        await buildApp('vue'),
-        await buildApp('react')
-    ];
+  await buildComponentLibrary();
+  await [
+    await buildApp('vue'),
+    await buildApp('react')
+  ];
 };
 
 buildAll();
