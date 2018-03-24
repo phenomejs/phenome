350815b6f0d013b7b9b5500d12f21bd349417933
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-03-24 15:59:18 +0300

    Use F7 bundle

--- test/app/react/src/app.js
+++ test/app/react/src/app.js
@@ -1,7 +1,7 @@
 /* eslint import/no-extraneous-dependencies: "off" */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Framework7 from 'framework7';
+import Framework7 from 'framework7/dist/framework7.esm.bundle';
 
 import App from './app-component';
 
--- test/app/vue/src/app.js
+++ test/app/vue/src/app.js
@@ -1,7 +1,7 @@
 /* eslint import/no-extraneous-dependencies: "off" */
 import Vue from 'vue';
-import Framework7 from 'framework7';
-import Framework7Vue from 'framework7-vue/dist/framework7-vue.esm.bundle';
+import Framework7 from 'framework7/dist/framework7.esm.bundle';
+import Framework7Vue from 'framework7-vue';
 
 import App from './app-component.vue';
 
