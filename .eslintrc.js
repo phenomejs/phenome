006c9645590ab38f635a749b371b7ff80b1bb3b4
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-04-05 22:05:44 +0300

    Transform react JSX

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-        'max-len': ['error', 200, 2, {
-            ignoreUrls: true,
-            ignoreComments: false,
-            ignoreRegExpLiterals: true,
-            ignoreStrings: true,
-            ignoreTemplateLiterals: true,
-          }],
-    }
+  "extends": "airbnb",
+  "rules": {
+    'max-len': ['error', 200, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
+    'no-param-reassign': ['error', {
+      props: false,
+    }]
+  }
 };
--- test-acorn/compile.js
+++ test-acorn/compile.js
@@ -1,5 +1,6 @@
 const acorn = require('acorn-jsx');
 const walk = require('acorn/dist/walk');
+const escodegen = require('escodegen');
 
 const content = `
 import Utils from '../utils/utils';
@@ -22,8 +23,15 @@ export default {
     const objectWithSpread = { ...someObject, baz: 'test3' };
 
     return (
-      <div className={this.test + 5}>Hello
-        <slot></slot>
+      <div id="test" className="someclass" rootBooleanProp readonly {...propsObj}>
+        <AnotherComponent booleanProp><p>Hello</p></AnotherComponent>
+        <slot>World</slot>
+        <slot name="header">Default header slot</slot>
+        here goes some text
+        here comes another text
+        <p>Inset expression is {this.someArray.join(' ')} good with inset <Component></Component></p>
+        <p>{someExpressionComesHere} with one more spread we like but doesn't make sense here {{foo: 'bar', ...wtf}}</p>
+        {propsObj ? (<span>Yes</span>) : (<span>No</span>)}
       </div>
     );
   },
@@ -41,20 +49,7 @@ export default {
   },
 };
 `;
-const ast = acorn.parse(content, {
-  sourceType: 'module',
-  ecmaVersion: '9',
-  plugins: { jsx: true },
-});
-
-walk.simple(ast, {
-  JSXElement(node) {
-    console.log('JSXElement', node);
-  },
-  JSXExpressionContainer(node) {
-    console.log('JSXExpressionContainer', node);
-  },
-}, {
+const base = {
   JSXElement(node, st, c) {
     node.children.forEach((n) => {
       c(n, st);
@@ -65,4 +60,154 @@ walk.simple(ast, {
   },
   JSXText() {},
   ...walk.base,
+};
+const ast = acorn.parse(content, {
+  sourceType: 'module',
+  ecmaVersion: '9',
+  plugins: { jsx: true },
 });
+
+function transformJSXText(node, parents) {
+  const textValue = node.value.trim().split('\n').map(line => line.trim()).join(' ');
+  const parentNode = parents[parents.length - 2];
+
+  if (!textValue) {
+    parentNode.children.splice(parentNode.children.indexOf(node), 1);
+  } else {
+    Object.assign(node, {
+      type: 'Literal',
+      raw: `'${textValue}'`,
+      value: textValue,
+    });
+  }
+}
+function transformJSXElement(node, parents) {
+  const all = acorn.parse(`React.createElement('div', {asd: true}, 'Yes')`)
+  const nullProps = acorn.parse(`React.createElement('div', null, 'Yes')`)
+  const component = acorn.parse(`React.createElement(MyPage, null, 'Yes')`)
+  const propsWithSpread = acorn.parse(`React.createElement(MyPage, {id: '2', ...propsObj}, 'Yes')`, {
+    ecmaVersion: '9',
+  });
+  const newNode = {
+    type: 'CallExpression',
+    callee: {
+      type: 'MemberExpression',
+      computed: false,
+      object: {
+        type: 'Identifier',
+        name: 'React',
+      },
+      property: {
+        type: 'Identifier',
+        name: 'createElement',
+      },
+    },
+    arguments: [],
+  };
+  const tagName = node.openingElement.name.name;
+  if (tagName.match(/[A-Z]/)) {
+    newNode.arguments.push({
+      type: 'Identifier',
+      name: tagName,
+    });
+  } else {
+    newNode.arguments.push({
+      type: 'Literal',
+      raw: `'${tagName}'`,
+      value: tagName,
+    });
+  }
+  if (node.openingElement.attributes.length === 0) {
+    newNode.arguments.push({
+      type: 'Literal',
+      raw: 'null',
+      value: null,
+    });
+  } else {
+    const propsObj = {
+      type: 'ObjectExpression',
+      properties: [],
+    };
+    node.openingElement.attributes.forEach((attr) => {
+      let prop;
+      if (attr.type === 'JSXAttribute') {
+        prop = {
+          type: 'Property',
+          computed: false,
+          kind: 'init',
+          method: false,
+          shorthand: false,
+          key: {
+            type: 'Identifier',
+            name: attr.name.name,
+          },
+        };
+        if (!attr.value) {
+          prop.value = {
+            type: 'Literal',
+            raw: 'true',
+            value: true,
+          };
+        } else if (attr.value.type === 'JSXExpressionContainer') {
+          prop.value = attr.value.expression;
+        } else {
+          prop.value = attr.value;
+        }
+      } else if (attr.type === 'JSXSpreadAttribute') {
+        prop = {
+          type: 'SpreadElement',
+          argument: attr.argument,
+        };
+      }
+      if (prop) propsObj.properties.push(prop);
+    });
+
+    newNode.arguments.push(propsObj);
+  }
+  if (node.children.length > 0) {
+    node.children.forEach((child) => {
+      if (child.type === 'JSXText') {
+        transformJSXText(child, [node]);
+      }
+      if (child.type === 'JSXExpressionContainer') {
+        transformJSXExpressionContainer(child, [node]);
+      }
+      newNode.arguments.push(child);
+    });
+  }
+  Object.assign(node, newNode);
+  delete node.children;
+  delete node.closingElement;
+  delete node.openingElement;
+}
+function transformJSXExpressionContainer(node, parents) {
+  const parentNode = parents[parents.length - 2];
+  parentNode.children[parentNode.children.indexOf(node)] = node.expression;
+  delete node.expression;
+}
+
+
+walk.ancestor(ast, {
+  JSXText(node, parents) {
+    transformJSXText(node, parents);
+  },
+}, base);
+
+walk.ancestor(ast, {
+  JSXExpressionContainer(node, parents) {
+    transformJSXExpressionContainer(node, parents);
+  },
+  JSXElement(node, parents) {
+    transformJSXElement(node, parents);
+  },
+}, base);
+
+const generated = escodegen.generate(ast, {
+  format: {
+    indent: {
+      style: '  ',
+    },
+    compact: false,
+  },
+});
+console.log(generated);
