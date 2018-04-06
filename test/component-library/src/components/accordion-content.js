bbfb05f7c5935c4540c08820cd7796989757f0b4
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-04-06 19:04:55 +0300

    Return back acc content

--- test/component-library/src/components/accordion-content.js
+++ test/component-library/src/components/accordion-content.js
@@ -5,20 +5,8 @@ export default {
   props: Mixins.colorProps,
   name: 'f7-accordion-content',
   render() {
-    const propsObj = {
-      id: this.props.id,
-      style: this.props.style,
-      className: this.classes,
-    };
-
-    const someArray = [1, 2, 3, 4];
-    const arrayWithSpread = [...someArray, 5, 6];
-
-    const someObject = { foo: 'test', bar: 'test2' };
-    const objectWithSpread = { ...someObject, baz: 'test3' };
-
     return (
-      <div {...propsObj}>
+      <div id={this.props.id} style={this.props.style} className={this.classes}>
         <slot></slot>
       </div>
     );
