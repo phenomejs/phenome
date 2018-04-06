762cc57f9b0e74cab06ba732a2b2eac9de312a59
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-04-06 19:16:17 +0300

    Fix vue jsx transform props

--- src/compilers/vue/jsx-transformer/runtime-helpers/transform-vue-jsx-props.js
+++ src/compilers/vue/jsx-transformer/runtime-helpers/transform-vue-jsx-props.js
@@ -1,25 +1,32 @@
 export default (data) => {
   if (!data) return data;
-  if (!data.attrs) return data;
-
-  Object.keys(data.attrs).forEach((key) => {
+  const nestedPropsKeys = ('style class domProps slot key ref').split(' ');
+  Object.keys(data).forEach((key) => {
     if (key === 'className') {
-      data.class = data.attrs.className;
-      delete data.attrs.className;
+      data.class = data.className;
+      delete data.className;
+      return;
+    } else if (key === 'dangerouslySetInnerHTML') {
+      if (!data.domProps) data.domProps = {};
+      data.domProps.innerHTML = data[key];
+      delete data.dangerouslySetInnerHTML;
+      return;
+    } else if (key.match(/^on?([A-Z])/)) {
+      if (!data.on) data.on = {};
+      const newKey = key.replace(/(^on?)([A-Z])/, (found, first, second) => second.toLowerCase());
+      data.on[newKey] = data[key];
+      delete data.key;
       return;
     }
-    if (key.indexOf('-') >= 0) return;
-
-    let newKey;
-    let value = data.attrs[key];
-    if (key === 'maxLength') newKey = 'maxlength';
-    else if (key === 'tabIndex') newKey = 'tabindex';
-    else {
-      newKey = key.replace(/([A-Z])/g, v => `-${v.toLowerCase()}`);
+    if (nestedPropsKeys.indexOf(key) >= 0) {
+      return;
     }
-    if (newKey !== key) {
-      data.attrs[newKey] = value;
-      delete data.attrs[key];
+    if (!data.attrs) {
+      data.attrs = {};
+    }
+    if (!data.attrs[key]) {
+      data.attrs[key] = data[key];
+      delete data.key;
     }
   });
 
