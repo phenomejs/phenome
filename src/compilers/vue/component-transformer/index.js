71e4f81bab1785176659d570b7307473590fd098
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-04-06 19:17:59 +0300

    all new vue-component-transform

--- src/compilers/vue/component-transformer/index.js
+++ src/compilers/vue/component-transformer/index.js
@@ -2,6 +2,7 @@
 /* eslint prefer-destructuring: "off" */
 /* eslint import/no-extraneous-dependencies: "off" */
 const codeToAst = require('../../compiler-utils/code-to-ast');
+const walk = require('../../compiler-utils/walk');
 const traversePhenomeComponent = require('../../compiler-utils/traverse-phenome-component');
 
 const getPropsFunctionCode = `
@@ -46,6 +47,9 @@ const addComputed = `
       slots() {
         return this.$slots;
       },
+      forceUpdate() {
+        return this.$forceUpdate();
+      },
     }
   }
 `;
@@ -53,23 +57,10 @@ const addMethods = `
   const obj = {
     methods: {
       dispatchEvent(events, ...args) {
-        const self = this;
-        events.split(' ').forEach((event) => {
-          self.$emit(event, ...args);
-        });
+        __vueComponentDispatchEvent(this, events, ...args);
       },
       setState(updater, callback) {
-        const self = this;
-        let newState;
-        if (typeof updater === 'function') {
-          newState = updater(self.state, self.props);
-        } else {
-          newState = updater;
-        }
-        Object.keys(newState).forEach((key) => {
-          self.$set(self.state, key, newState[key])
-        });
-        if (typeof callback === 'function') callback();
+        __vueComponentSetState(this, updater, callback);
       },
     }
   }
@@ -81,41 +72,39 @@ function state() {
   return { state };
 }
 `;
-function wrapComponentProps(declaration) {
-  // Collect Props
-  declaration.properties.forEach((prop) => {
-    if (prop.key && prop.key.name === 'props') {
+
+function modifyVueComponent(componentNode, config, requiredHelpers) {
+  let computed;
+  let methods;
+
+  traversePhenomeComponent(componentNode, {
+    props(node) {
+      if (!requiredHelpers.props) return;
       const newValue = {
         type: 'CallExpression',
         callee: {
           type: 'Identifier',
           name: '__getVueComponentPropKeys',
         },
-        arguments: [prop.value],
+        arguments: [node.value],
       };
-      prop.value = newValue;
-    }
-  });
-}
-function modifyVueComponent(declaration) {
-  let computed;
-  let methods;
-  let hasProps;
-
-  traversePhenomeComponent(declaration, {
-    props() {
-      hasProps = true;
+      node.value = newValue;
     },
     state(node) {
       node.key.name = 'data';
-
-      if (node.params && node.params.length > 0) {
-        node.params.splice(0, 1);
+      if (!requiredHelpers.state) {
+        return;
+      }
+      if (node.value.params && node.value.params.length > 0) {
+        node.value.params.splice(0, 1);
       }
 
-      const stateFunctionNode = codeToAst(stateFunctionCode).program.body[0];
-      stateFunctionNode.body.body[1].declarations[0].init.callee.body.body.push(...node.body.body);
-      node.body.body = stateFunctionNode.body.body;
+      const stateFunctionNode = codeToAst(stateFunctionCode).body[0];
+      stateFunctionNode.body.body[1].declarations[0].init.callee.body.body.push(...node.value.body.body);
+      if (!requiredHelpers.props) {
+        stateFunctionNode.body.body.splice(0, 1);
+      }
+      node.value.body = stateFunctionNode.body;
     },
     computed(node) {
       computed = node;
@@ -146,40 +135,128 @@ function modifyVueComponent(declaration) {
     },
   });
 
-  // Add/Modify Computed Props
-  const computedObjToAdd = codeToAst(addComputed).program.body[0].declarations[0].init.properties[0];
-  if (computed) {
-    const computedPropsToAdd = computedObjToAdd.value.properties;
-    computed.value.properties.push(...computedPropsToAdd);
-  } else {
-    declaration.properties.push(computedObjToAdd);
+  // Add/Modify Computed Props Helpers
+  const computedObjToAdd = codeToAst(addComputed).body[0].declarations[0].init.properties[0];
+  if (!computed) {
+    computed = {
+      type: 'Property',
+      method: false,
+      kind: 'init',
+      computed: false,
+      key: {
+        name: 'computed',
+        type: 'Identifier',
+      },
+      value: {
+        type: 'ObjectExpression',
+        properties: [],
+      },
+    };
+    componentNode.properties.push(computed);
+  }
+  computedObjToAdd.value.properties.forEach((helperProp) => {
+    if (requiredHelpers[helperProp.key.name]) {
+      computed.value.properties.push(helperProp);
+    }
+  });
+  if (computed.value && computed.value.properties.length === 0) {
+    componentNode.properties.splice(componentNode.properties.indexOf(computed), 1);
   }
 
-  // Add/Modify Methods Props
-  const methodsObjToAdd = codeToAst(addMethods).program.body[0].declarations[0].init.properties[0];
-  if (methods) {
-    const methodPropsToAdd = methodsObjToAdd.value.properties;
-    methods.value.properties.push(...methodPropsToAdd);
-  } else {
-    declaration.properties.push(methodsObjToAdd);
+  // Add/Modify Methods Props Helpers
+  const methodsObjToAdd = codeToAst(addMethods).body[0].declarations[0].init.properties[0];
+  if (!methods) {
+    methods = {
+      type: 'Property',
+      method: false,
+      kind: 'init',
+      computed: false,
+      key: {
+        name: 'methods',
+        type: 'Identifier',
+      },
+      value: {
+        type: 'ObjectExpression',
+        properties: [],
+      },
+    };
+    componentNode.properties.push(methods);
   }
-
-  return {
-    hasProps,
-  };
+  methodsObjToAdd.value.properties.forEach((helperProp) => {
+    if (requiredHelpers[helperProp.key.name]) {
+      methods.value.properties.push(helperProp);
+    }
+  });
+  if (methods.value && methods.value.properties.length === 0) {
+    componentNode.properties.splice(componentNode.properties.indexOf(methods), 1);
+  }
 }
 
-function transform(name, componentNode, state) {
-  const { hasProps } = modifyVueComponent(componentNode);
+function findHelpers(ast, componentNode, config, jsxHelpers) {
+  const thisAliases = ('this that self component').split(' ');
+  const lookForHelpers = ('el slots props children parent refs dispatchEvent setState state forceUpdate').split(' ');
+  const foundHelpers = [];
 
+  walk(ast, {
+    MemberExpression(node) {
+      if (
+        (
+          (node.object.type === 'ThisExpression') ||
+          (node.object.type === 'Identifier' && thisAliases.indexOf(node.object.name) >= 0)
+        ) &&
+        node.property.type === 'Identifier' &&
+        lookForHelpers.indexOf(node.property.name) >= 0 &&
+        foundHelpers.indexOf(node.property.name) < 0
+      ) {
+        foundHelpers.push(node.property.name);
+      }
+      if (node.object.type === 'Identifier' && node.object.name === 'props' && foundHelpers.indexOf('props') < 0) {
+        foundHelpers.push('props');
+      }
+      if (node.object.type === 'Identifier' && node.object.name === 'state' && foundHelpers.indexOf('state') < 0) {
+        foundHelpers.push('state');
+      }
+    },
+  });
+
+  traversePhenomeComponent(componentNode, {
+    props() {
+      if (foundHelpers.indexOf('props') < 0) foundHelpers.push('props');
+    },
+  });
+
+  const helpers = {};
+  const configHelpers = config.helpers;
+  lookForHelpers.forEach((helper) => {
+    if (configHelpers && configHelpers[helper] === false) return;
+    if (configHelpers && configHelpers[helper] === true) helpers[helper] = true;
+    if (!configHelpers || (configHelpers && (configHelpers[helper] === 'auto' || typeof configHelpers === 'undefined'))) {
+      if (jsxHelpers && jsxHelpers[helper] === true) helpers[helper] = true;
+      if (foundHelpers.indexOf(helper) >= 0) helpers[helper] = true;
+    }
+  });
+
+  return helpers;
+}
+function transform(ast, name, componentNode, state, config, jsxHelpers) {
+  const requiredHelpers = findHelpers(ast, componentNode, config, jsxHelpers);
+
+  modifyVueComponent(componentNode, config, requiredHelpers);
+
+  if (requiredHelpers.setState) {
+    state.addRuntimeHelper('__vueComponentSetState', './runtime-helpers/vue-component-set-state.js');
+  }
+  if (requiredHelpers.dispatchEvent) {
+    state.addRuntimeHelper('__vueComponentDispatchEvent', './runtime-helpers/vue-component-dispatch-event.js');
+  }
   // Add props
-  if (hasProps) {
-    wrapComponentProps(componentNode);
-    const getPropsFunctionNodes = codeToAst(getPropsFunctionCode).program.body;
+  if (requiredHelpers.props) {
+    // wrapComponentProps(componentNode);
+    const getPropsFunctionsNodes = codeToAst(getPropsFunctionCode).body;
 
-    state.addDeclaration('__vueComponentPropKeys', getPropsFunctionNodes[0]);
-    state.addDeclaration('__getVueComponentPropKeys', getPropsFunctionNodes[1]);
-    state.addDeclaration('__getVueComponentProps', getPropsFunctionNodes[2]);
+    state.addDeclaration('__vueComponentPropKeys', getPropsFunctionsNodes[0]);
+    state.addDeclaration('__getVueComponentPropKeys', getPropsFunctionsNodes[1]);
+    state.addDeclaration('__getVueComponentProps', getPropsFunctionsNodes[2]);
   }
 
   return componentNode;
