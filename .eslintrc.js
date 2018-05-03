5f181e924ebc34a9a47e3e09c22fd9688ee73745
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-05-03 12:54:53 +0300

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,20 @@
 module.exports = {
-  "extends": "airbnb",
-  "rules": {
-    'max-len': ['error', 200, 2, {
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    }],
-    'no-param-reassign': ['error', {
-      props: false,
-    }],
-    'prefer-destructuring': 'off',
-    'object-curly-newline': ['error', {
-      ObjectExpression: { minProperties: 8, multiline: true, consistent: true },
-      ObjectPattern: { minProperties: 8, multiline: true, consistent: true }
-    }],
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/no-string-refs': 'off',
-    'react/jsx-no-bind': 'off',
-    'react/jsx-closing-tag-location': 'off',
-  }
+    "extends": "airbnb-base",
+    "rules": {
+        'max-len': ['error', 200, 2, {
+            ignoreUrls: true,
+            ignoreComments: false,
+            ignoreRegExpLiterals: true,
+            ignoreStrings: true,
+            ignoreTemplateLiterals: true,
+        }],
+        'no-param-reassign': ['error', {
+            props: false,
+        }],
+        'prefer-destructuring': 'off',
+        'object-curly-newline': ['error', {
+            ObjectExpression: { minProperties: 8, multiline: true, consistent: true },
+            ObjectPattern: { minProperties: 8, multiline: true, consistent: true },
+        }],
+    }
 };
