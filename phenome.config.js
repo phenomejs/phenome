6b11b0b68718a3f0177e3a881f7fe65e82e8b104
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-08-28 22:34:02 +0300

    Finalize React d.ts

--- phenome.config.js
+++ phenome.config.js
@@ -2,7 +2,7 @@ module.exports = {
   paths: ['./test-component/src/**/*.js', './test-component/src/**/*.jsx'],
   react: {
     out: './test-component/dist/react/',
-    generateTypeScriptDefinitions: true,
+    typeScriptDefinitions: true,
     helpers: {
       el: 'auto',
       slots: 'auto',
