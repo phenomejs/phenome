ba9128e5e47d356fbafe1828a4434f918c4138bc
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-09-22 13:05:15 +0300

    Add support for extra output in generated TS declarations
    Ref #11

--- test-component/src/component.jsx
+++ test-component/src/component.jsx
@@ -1,5 +1,10 @@
 import Mixins from './mixins';
-
+/* phenome-dts-imports
+import Something from 'somewhere';
+*/
+/* phenome-react-dts-imports
+import SomethingReact from 'somewhere-react';
+*/
 const moreProps = {
   moreFoo: String,
 };
@@ -10,6 +15,11 @@ const deepProps = {
     },
   }
 }
+
+/* phenome-dts-instance
+  instanceFoo: String
+  instanceFoo2: String
+*/
 
 export default {
   name: 'my-phenome-component',
@@ -27,6 +37,13 @@ export default {
     ...moreProps,
     ...deepProps.moreDeepProps.eventMoreDeepProps,
     ...Mixins.colorProps,
+    /* phenome-dts-props
+    fooDts: string
+    fooDts2: string
+    */
+    /* phenome-react-dts-props
+    reactFoo: string
+    */
   },
   methods: {
     open(animate = true) {
